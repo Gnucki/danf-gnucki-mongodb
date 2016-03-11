74f354c0bdedf47eb1e5570a1fd7d9518084a4df
Thomas Prelot <tprelot@gmail.com>
2016-03-11 17:44:44 +0100

    Add driver db proxy

--- config/server/config/interfaces.js
+++ config/server/config/interfaces.js
@@ -3,10 +3,90 @@
 module.exports = {
     db: {
         methods: {
-            listCollections: {}
+            // Proxy on driver interface.
+            // Take a look at http://mongodb.github.io/node-mongodb-native/
+            // for latest API documentation.
+            addUser: {
+                arguments: ['string/username', 'string/password', 'object|null/options']
+            },
+            admin: {
+                returns: 'object'
+            },
+            authenticate: {
+                arguments: ['string/username', 'string/password', 'object|null/options']
+            },
+            close: {
+                arguments: ['boolean/force']
+            },
+            collection: {
+                arguments: ['string/name', 'object|null/options']
+            },
+            command: {
+                arguments: ['name/command', 'object|null/options']
+            },
+            createCollection: {
+                arguments: ['string/name', 'object|null/options']
+            },
+            createIndex: {
+                arguments: ['string/name', 'string|object/fieldOrSpec', 'object|null/options']
+            },
+            db: {
+                arguments: ['string/name', 'object|null/options'],
+                returns: 'object'
+            },
+            dropCollection: {
+                arguments: ['string/name']
+            },
+            dropDatabase: {},
+            executeDbAdminCommand: {
+                arguments: ['name/command', 'object|null/options']
+            },
+            indexInformation: {
+                arguments: ['string/name', 'object|null/options']
+            },
+            listCollections: {
+                arguments: ['object|null/filter', 'object|null/options'],
+                returns: 'object'
+            },
+            logout: {
+                arguments: ['object|null/options']
+            },
+            removeUser: {
+                arguments: ['string/username', 'object|null/options']
+            },
+            stats: {
+                arguments: ['object|null/options']
+            },
         },
         getters: {
-            name: 'string'
+            /**
+             * The identifier name of the database.
+             *
+             * @var string
+             */
+            id: 'string',
+            /**
+             * The name of the database.
+             *
+             * @var string
+             */
+            name: 'string',
+            /**
+             * The related driver db object.
+             *
+             * @var string
+             */
+            driver: 'object',
+            // Proxy on driver interface.
+            // Take a look at http://mongodb.github.io/node-mongodb-native/
+            // for latest API documentation.
+            serverConfig: 'object',
+            bufferMaxEntries: 'number',
+            databaseName: 'string',
+            options: 'object',
+            native_parser: 'boolean',
+            slaveOk: 'boolean',
+            writeConcern: 'object'
         }
     },
     collection: {
